e2ce1a623111abe5949c2ea2751a0ba5426cb383
The Noah <35819660+The-Noah-BZ@users.noreply.github.com>
2018-11-18 14:40:09 -0600

    Update cat command

--- commands/cat.js
+++ commands/cat.js
@@ -3,9 +3,9 @@ const fetch = require("node-fetch");
 module.exports = {
   name: "cat",
   description: "Show a random cat photo",
-  aliases: ["cat-photo", "cat-image"],
+  aliases: ["cat-photo", "cat-image", "meow", "kitten"],
   usage: "[@user ...]",
-  cooldown: 10,
+  cooldown: 5,
   execute(message, args){
     message.reply("Please wait...").then(msg => {
       fetch('https://aws.random.cat/meow').then(response => response.json()).then(body => {
