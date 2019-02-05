151758549ff3fcb9835452c81a96d9e1a0304dd4
The Noah <thenoahbz@gmail.com>
2019-02-04 21:33:40 -0600

    Remove args-info command

--- commands/args-info.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "args-info",
-  description: "Display all of the arguments given",
-  args: true,
-  usage: "<args>",
-  execute(message, args){
-    message.reply(`Arguments: \`${args.join(", ")}\``);
-  },
-};
